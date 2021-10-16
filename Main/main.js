b533455358382a74043a98964d54a05046cbf1d1
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-10-16 10:57:53 +0530

    Update main.js

--- Main/main.js
+++ Main/main.js
@@ -1,46 +1,47 @@
-let url = "http://api.mediastack.com/v1/news?access_key=53401c2ce96d5ab09bd48acc762335f7&countries=in&category=health;
-let Init = {
-  method: "Get",
-  headers: {
-    //not allowed
-  },
-};
-
-let request = new Request(url, Init);
-
-displayNews = (articles) => {
-  console.log("displayNews called");
-
-  return new Promise((resolve, reject) => {
-    try {
-      let news = "";
-        news += `<div class="news-container">
+// api url
+const url = "http://api.mediastack.com/v1/news?access_key=53401c2ce96d5ab09bd48acc762335f7&countries=in&category=health";
+  
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Name</th>
+          <th>Office</th>
+          <th>Position</th>
+          <th>Salary</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data.list) {
+        tab += `<div class="news-container">
         <div class="source">Source: ${data.source}</div>
         <div class="date">${data.published_at}</div>
         <div class="title">${data.title}</div>
         <div class="description">${data.description}</div>       
         <a href="${data.url}" target="_blank">Read more</a>
-      </div>`;
-      });
-      document.getElementsByClassName("news")[0].innerHTML = news;
-
-      resolve("News Displayed Sucessfully");
-    } catch (err) {
-      reject(err);
+      </div>`;;
     }
-  });
-};
-
-(fetchData = () => {
-  console.log("fetchData() called");
-
-  fetch(request)
-    .then((response) => response.json())
-    .then((responseObj) => {
-      console.log("Parsed Response Received");
-
-      return displayNews(responseObj.articles);
-    })
-    .then((message) => console.log(message))
-    .catch((err) => console.log(err));
-})();
+    // Setting innerHTML as tab variable
+    document.getElementById("employees").innerHTML = tab;
+}
