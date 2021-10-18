5e6c2695fdb0ea33f431e57306942d4b1031dc89
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-10-18 11:27:37 +0530

    Update main.js

--- Main/main.js
+++ Main/main.js
@@ -1,47 +1,40 @@
-// api url
-const api_url = "http://api.mediastack.com/v1/news?access_key=53401c2ce96d5ab09bd48acc762335f7&countries=in&category=health";
-  
-// Defining async function
-async function getapi(url) {
-    
-    // Storing response
-    const response = await fetch(url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const country = "in";
+const category = "health";
+const apiKey = "cf50991ed60447479fbc637a21e15425";
+const url = `${proxyUrl}https://newsapi.org/v2/top-headlines?countries=${country}&category=${category}&apiKey=${apiKey}`;
+const request = new Request(url);
+
+fetch(request)
+  .then(response => response.json())
+  .then((news) => {
+    console.log(news);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+let request = new Request(url);
+
+displayNews = (articles) => {
+  console.log("displayNews called");
+
+  return new Promise((resolve, reject) => {
+    try {
+      let news = "";
+      articles.forEach((article) => {
+        news += `<div class="news-container">
+        <div class="source">Source: ${article.source.name}</div>
+        <div class="date">${article.publishedAt.slice(0, 10)}</div>
+        <div class="title">${article.title}</div>
+        <div class="description">${article.description}</div>       
+        <a href="${article.url}" target="_blank">Read more</a>
+      </div>`;
+      });
+      document.getElementsByClassName("news")[0].innerHTML = news;
+
+      resolve("News Displayed Sucessfully");
+    } catch (err) {
+      reject(err);
     }
-    show(data);
-}
-// Calling that async function
-getapi(api_url);
-  
-// Function to hide the loader
-function hideloader() {
-    document.getElementById('loading').style.display = 'none';
-}
-// Function to define innerHTML for HTML table
-function show(data) {
-    let tab = 
-        `<tr>
-          <th>Name</th>
-          <th>Office</th>
-          <th>Position</th>
-          <th>Salary</th>
-         </tr>`;
-    
-    // Loop to access all rows 
-    for (let r of data.list) {
-        tab += `<div class="news-container">
-        <div class="source">Source: ${data.source}</div>
-        <div class="date">${data.published_at}</div>
-        <div class="title">${data.title}</div>
-        <div class="description">${data.description}</div>       
-        <a href="${data.url}" target="_blank">Read more</a>
-      </div>`;;
-    }
-    // Setting innerHTML as tab variable
-    document.getElementsByClassName("news").innerHTML = tab;
-}
+  });
+};
